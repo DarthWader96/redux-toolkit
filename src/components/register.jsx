c062f03b8392ff47536988d9a294b7767456c9d1
DarthWader96 <shaokahn848@gmail.com>
2026-05-23 22:55:42 +0500

    2. API. Redux register

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react"
 import { logo } from "../constants"
 import { Input } from "../ui"
+import { useDispatch, useSelector } from "react-redux"
+import { registerUserStart } from "../slice/auth"
 const Register = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const {isLoading} = useSelector(state=> state.auth)
+
+  const loginHandler = (e) => {
+    e.preventDefault()
+    dispatch(registerUserStart())
+  }
+
   return (
     <div className="text-center mt-5">
       <main className="form-signin w-25 m-auto">
@@ -16,7 +26,7 @@ const Register = () => {
           <Input label={"Email address"} type={"email"} state={email} setState={setEmail}/>
           <Input label={"Password"} type={"password"} state={password} setState={setPassword}/>
 
-          <button className="btn btn-primary w-100 py-2 mt-2" type="submit">Register</button>
+          <button className="btn btn-primary w-100 py-2 mt-2" disabled={isLoading} onClick={loginHandler} type="submit">{isLoading ? "Loading..." : "Register"}</button>
         </form>
       </main>
     </div>
