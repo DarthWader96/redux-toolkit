cb23edc779978291c01f47cf2e8dc1eb8d21fb09
DarthWader96 <shaokahn848@gmail.com>
2026-05-24 22:29:53 +0500

    23.Article detail UI

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react"
 import { useParams } from "react-router"
 import ArticleService from "../service/article"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from "../slice/article"
+import moment from "moment"
+import { Loader } from "../ui"
 const ArticleDetail = () => {
     const { slug } = useParams()
     const dispatch = useDispatch()
+    const { articleDetail, isLoading } = useSelector(state => state.article)
 
     const getArticleDetail = async () => {
         dispatch(getArticleDetailStart())
@@ -21,8 +24,36 @@ const ArticleDetail = () => {
         getArticleDetail()
     }, [slug])
 
-    return (
-        <div>id: {slug}</div>
+    return isLoading ? <Loader/> : (
+        articleDetail !== null && (
+            <div>
+                <div className="p-5 mb-4 rounded-3">
+                    <div className="container-fluid py-5">
+                        <h1 className="display-5 fw-bold">{articleDetail.title}</h1>
+                        <p className="col-md-8 fs-4">{articleDetail.description}</p>
+                        <p className="text-muted">
+                            <span className="fw-bold">Created at:</span> {moment(articleDetail.createdAt).format("DD MMM YYYY")}
+                        </p>
+                        <div className="col-md-6">
+                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                <div className="col p-4 d-flex flex-column position-static">
+                                    <strong className="d-inline-block mb-2 text-primary text-uppercase">{articleDetail.author.username}</strong>
+                                    <p className="card-text mb-auto">{articleDetail.author.bio}</p>
+                                </div>
+                                <div className="col-auto d-none d-lg-block">
+                                    <svg aria-label="Placeholder: Thumbnail" className="bd-placeholder-img " height="250" preserveAspectRatio="xMidYMid slice" role="img" width="200" xmlns="http://www.w3.org/2000/svg">
+                                        <title>Placeholder</title>
+                                        <rect width="100%" height="100%" fill="#55595c"></rect>
+                                        <text x="45%" y="53%" fill="#eceeef" className="fs-2 text-uppercase p-0 m-0">{articleDetail.author.username[0]}</text>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div>{articleDetail.body}</div>
+                    </div>
+                </div>
+            </div>
+        )
     )
 }
 
