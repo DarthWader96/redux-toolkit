903c7f7a957be9dd4ca9655705cb65fbb5462a08
DarthWader96 <shaokahn848@gmail.com>
2026-05-24 23:01:08 +0500

    24.Create article. UI

---
+++ src/components/create-article.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { Input, TextArea } from "../ui"
+
+const CreateArticle = () => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [body, setBody] = useState('')
+    return (
+        <div className="text-center">
+            <h1 className="fs-2">Create Article</h1>
+            <div className="w-75 mx-auto">
+                <form>
+                    <Input label={"title"} state={title} setState={setTitle}/>
+                    <TextArea label={"Description"} state={description} setState={setDescription}/>
+                    <TextArea label={"Body"} state={body} setState={setBody} height={"300px"}/>
+                    <button className="btn btn-primary w-100 py-2 mt-2" type="submit">Create</button>
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+export default CreateArticle
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,6 +25,7 @@ const Navbar = () => {
                 {loggedIn ? (
                     <>
                         <p className="me-3 py-2 m-0">{user.username}</p>
+                        <Link to={"/create-article"} className="me-3 py-2 link-body-emphasis text-decoration-none">Create</Link>
                         <button className="btn btn-outline-danger" onClick={logoutHandler}>Logout</button>
                     </>
                 ) : (
